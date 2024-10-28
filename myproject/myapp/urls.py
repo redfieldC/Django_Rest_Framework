60145f2ae408e0de315ce8ccb60e6db22ce2e15c
Ameya Potdar <ameyapradippotdar@gmail.com>
2024-10-28 12:24:44 +0530

    completed django rest framework

---
+++ myproject/myapp/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path,include
+from . import views 
+urlpatterns = [
+    path('api/register/', views.register_user, name='register'),
+    path('api/login/', views.login_user, name='login'),
+    path('api/logout/', views.logout_user, name='logout'),
+    # path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    # path('api/logout/', views.logout_user, name='logout'),
+]
---
+++ myproject/myapp/views.py
@@ -0,0 +1,60 @@
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework_simplejwt.tokens import RefreshToken,AccessToken
+
+
+@api_view(['POST'])
+def register_user(request):
+    username = request.data.get('username')
+    password = request.data.get('password')
+    email = request.data.get('email')
+
+    if not username or not password or not email:
+        return Response({'error': 'Please provide username, password, and email'}, status=status.HTTP_400_BAD_REQUEST)
+
+    if User.objects.filter(username=username).exists():
+        return Response({'error': 'Username already exists'}, status=status.HTTP_400_BAD_REQUEST)
+
+    user = User.objects.create_user(username=username, password=password, email=email)
+    user.save()
+
+    return Response({'message': 'User registered successfully'}, status=status.HTTP_201_CREATED)
+
+
+
+
+@api_view(['POST'])
+def login_user(request):
+    username = request.data.get('username')
+    password = request.data.get('password')
+
+    # Authenticate the user
+    user = authenticate(username=username, password=password)
+
+    if user is not None:
+        # Create access and refresh tokens
+        refresh = RefreshToken.for_user(user)
+        access = str(refresh.access_token)
+
+        return Response({
+            'refresh': str(refresh),
+            'access': access,
+            'username': user.username,
+            'email': user.email,
+        }, status=status.HTTP_200_OK)
+    else:
+        return Response({'error': 'Invalid credentials'}, status=status.HTTP_401_UNAUTHORIZED)
+
+
+@api_view(['POST'])
+def logout_user(request):
+    try:
+        refresh_token = request.data["refresh"]
+        token = RefreshToken(refresh_token)
+        token.blacklist()
+        return Response({'message': 'Logged out successfully'}, status=status.HTTP_205_RESET_CONTENT)
+    except Exception as e:
+        return Response({'error': 'Bad Request'}, status=status.HTTP_400_BAD_REQUEST)
